35b96e4672bdad8c8ec752f6fe1c9be8ae9bdb0d
pattn <pattn@SUSHREE>
2024-12-29 13:16:53 +0530

    Add project files.

---
+++ azureSqlDbLearn/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using azureSqlDbLearn.Data;
+
+namespace azureSqlDbLearn.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly azureSqlDbLearn.Data.AppDbContext _context;
+
+        public IndexModel(azureSqlDbLearn.Data.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Person = await _context.Persons.ToListAsync();
+        }
+    }
+}
